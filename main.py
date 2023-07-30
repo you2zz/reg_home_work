b9fa8e00c3b7e0770bf4314419da7811869b8879
You2zz <yri_vla@mail.ru>
2023-07-31 00:24:03 +0500

    31072023_0023

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ with open("phonebook_raw.csv",encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
 pprint(contacts_list)
+
+
+#(\+7|8)?\s*\(?(\d{3})\)?[-\s]*(\d{3})[-\s]*(\d{2})[-\s]*(\d{2})\s*\(*(доб.)\s*(\d+)*\)*
+#+7(\2)\3-\4-\5 \6\7
