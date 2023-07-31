d7584dafee49d9ad028c3f1a32955f8ae8ed6a83
you2zz <yri_vla@mail.ru>
2023-07-31 17:29:48 +0500

    31072023_1729

--- main.py
+++ main.py
@@ -1,42 +1,10 @@
 from pprint import pprint
+import re
 ## Читаем адресную книгу в формате CSV в список contacts_list:
 import csv
-with open("phonebook_raw.csv",encoding='utf-8') as f:
+with open("phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-# pprint(contacts_list)
-
-# contacts_list2=[]
-
-# for i in contacts_list:
-#   new_i = ['', '', '', '', '', '', '']
-#   i_0 = i[0].split()
-#   i_1 = i[1].split()
-#   if len(i_0) == 3:
-#     for idx, _ in enumerate(new_i):
-#       if idx < 3:
-#         new_i[idx] = i_0[idx].strip()
-#       else:
-#         new_i[idx] = i[idx]    
-#   elif len(i_0) == 2:
-#     for idx, _ in enumerate(new_i):
-#       if idx < 2:
-#         new_i[idx] = i_0[idx].strip()
-#       else:
-#         new_i[idx] = i[idx]   
-#   else: 
-#     if len(i_1) == 2:
-#       for idx, _ in enumerate(new_i):
-#         if idx < 3 and idx != 0:
-#           new_i[idx] = i_1[idx - 1].strip()
-#         else:
-#           new_i[idx] = i[idx]
-#     else:
-#       for idx, _ in enumerate(new_i):
-#         new_i[idx] = i[idx]
-#   contacts_list2.append(new_i)
-
-# pprint(contacts_list2)
 
 surnames = {}
 organizations = {}
@@ -44,12 +12,15 @@ positions = {}
 phones = {}
 emails = {}
 
+pattern = r"(\+7|8)?\s*\(?(\d{3})\)?[-\s]*(\d{3})[-\s]*(\d{2})[-\s]*(\d{2})\s*\(*(доб.)*\s*(\d+)*\)*"
+replace = r"+7(\2)\3-\4-\5 \6\7"
+
 for i in contacts_list:
   i_0 = i[0].split()
   i_1 = i[1].split()
   organization = i[3]  
   position = i[4]  
-  phone = i[5] 
+  phone = re.sub(pattern, replace, i[5])
   email = i[6] 
   if len(i_0) == 3:
     last_first_name = f'{i_0[0].strip()} {i_0[1].strip()}'
@@ -91,6 +62,9 @@ for i in contacts_list:
     if email != '':
       emails[last_first_name] = email
 
+
+
+
 new_contact_list = []
 for k, v in surnames.items():
   entry = []
@@ -104,20 +78,3 @@ for k, v in surnames.items():
   new_contact_list.append(entry)
 
 print(new_contact_list)
-
-
-
-  
-# print(len(surnames))
-# print(len(organizations))
-# print(len(positions))
-# print(len(phones))
-# print(len(emails))
-
-
-
-
-
-
-#(\+7|8)?\s*\(?(\d{3})\)?[-\s]*(\d{3})[-\s]*(\d{2})[-\s]*(\d{2})\s*\(*(доб.)*\s*(\d+)*\)*
-#+7(\2)\3-\4-\5 \6\7
